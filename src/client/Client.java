bdc5ad39459aface801565497cbb0dfe3ca268b0
yasiga <62150844+yasiga@users.noreply.github.com>
2020-06-01 19:27:39 +0200

    Update Client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,6 @@
 package client;
 
-//import home.Home;
+import home.Home;
 import user.User;
 
 import javax.swing.*;
@@ -12,6 +12,10 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Client {
 	
@@ -64,7 +68,7 @@ public class Client {
 
 
 	public static void Connect(){
-		//user = Home.user;
+		user = Home.user;
 
 		try{
 			final int port = 2222;
@@ -98,7 +102,237 @@ public class Client {
 	
 	
 	
+	
+	
+	public static void BuildGroupWindow(){
+		mainWindow.setTitle("Project ChatRoom - "+ user.getUserName());
+		
+		ConfigureMainWindow();
+		MainWindow_Action();
+		mainWindow.setVisible(true);
+	}
+	
+	
+	
+	
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void ConfigureMainWindow(){
+		
+		
+		//setting MainWindow
+		mainWindow.setContentPane(gui);
+		mainWindow.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		mainWindow.setMinimumSize(new Dimension(500,300));
+		mainWindow.pack();
+		mainWindow.setLocationRelativeTo(null);
+		try {
+			// 1.6+
+			mainWindow.setLocationByPlatform(true);
+			mainWindow.setMinimumSize(mainWindow.getSize());
+		} 
+		catch(Throwable ignoreAndContinue) {
+		}
+		
+		
+		//setting top bar
+		top.setText("Offline");
+		
+		final UIManager.LookAndFeelInfo[] themes = UIManager.getInstalledLookAndFeels();     
+        themeNames = new String[themes.length];
+        int ii;
+        for (ii=0; ii<themes.length; ii++) {
+                themeNames[ii] = themes[ii].getName();
+        }
+        
+        PLAFContainer.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
+        
+        themeChooser = new JComboBox(themeNames);
+        themeChooser.setSelectedIndex(ii-1);
+        
+        
+        
+        PLAFContainer.add(themeChooser);
+        
+        topBar.setLayout(new BorderLayout(5,5));
+		topBar.setBorder(new TitledBorder(""));
+        topBar.add(top, BorderLayout.WEST);
+        topBar.add(PLAFContainer, BorderLayout.EAST);
+              
+        
+        //setting the username list
+        listScroll.setHorizontalScrollBarPolicy(
+        		ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        listScroll.setVerticalScrollBarPolicy(
+        		ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        listScroll.setViewportView(userOnlineList);
+        listScroll.setPreferredSize(new Dimension(130,200));
+        listScroll.setMinimumSize(new Dimension(130,200));
+        
+        
+        //setting the submit Button
+        submit.setText("SEND");
+        submit.setPreferredSize(new Dimension(100,100));
+        submit.setMinimumSize(new Dimension(100,30));
+        
+        //setting the east
+        userList.setLayout(new BorderLayout(5,5));
+        userList.add(listScroll,BorderLayout.CENTER);
+        userList.add(submit,BorderLayout.SOUTH);
+                
+        
+        //setting the chat display area
+        displayText.setText("");
+        displayText.setBorder(new LineBorder(Color.GRAY));
+        displayText.setEditable(false);
+            
+        
+        
+        //setting the textarea to type chat
+        typeText.setPreferredSize(new Dimension(400,60));
+        typeText.setEditable(true);
+        typeText.setBorder(new LineBorder(Color.GRAY));
+        
+        buttonText.setLayout(new BorderLayout(5,5));
+		buttonText.add(new JScrollPane(typeText),BorderLayout.CENTER);
+		
+        buttonLabelText.setLayout(new BorderLayout(5,5));
+		buttonLabelText.add(buttonText,BorderLayout.CENTER);
+		buttonLabelText.add(message,BorderLayout.WEST);
+		
+		
+        textCenter.setLayout(new BorderLayout(5,5));
+        textCenter.add(new JScrollPane(displayText), BorderLayout.CENTER);
+		textCenter.add(buttonLabelText,BorderLayout.SOUTH);
+		
+		
+		//setting everything in gui
+		gui.setLayout(new BorderLayout(5,5));
+		gui.add(topBar, BorderLayout.NORTH);
+        gui.add(userList, BorderLayout.EAST);
+		gui.add(textCenter,BorderLayout.CENTER);
 
+	}
+
+	
+	
+	
+	
+	
+	public static void MainWindow_Action(){
+
+		mainWindow.addWindowListener(
+			new WindowAdapter(){
+				public void windowClosing(WindowEvent e){
+				       int result = JOptionPane.showConfirmDialog(null, "Are you sure","Confirm",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+
+				       if(result == JOptionPane.YES_OPTION){
+				        	try {
+								output.close();
+								clientThread.in.close();
+							} catch (IOException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+				        	
+				        	
+							System.exit(0);
+						} else{
+				               //Do nothing
+				       }
+				 }
+			}
+		);
+		
+		submit.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					try {
+						SUBMIT_ACTION();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}					
+				}
+				
+			}
+		);
+		
+		userOnlineList.addMouseListener(
+			new MouseAdapter(){
+				public void mouseClicked(MouseEvent e) {
+			        USERONLINELIST_ACTION(e);
+				}
+			}
+		);
+		
+		themeChooser.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent ae) {
+					int index = themeChooser.getSelectedIndex();
+					final UIManager.LookAndFeelInfo[] themes = UIManager.getInstalledLookAndFeels();
+					try {
+						UIManager.setLookAndFeel( themes[index].getClassName() );
+						SwingUtilities.updateComponentTreeUI(mainWindow);
+						
+					} catch(Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		);
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	public static void USERONLINELIST_ACTION(MouseEvent e){
+		if (e.getClickCount() == 2) {
+			final String selectedUser = (String) userOnlineList.getSelectedValue();
+			SwingUtilities.invokeLater(
+				new Runnable(){
+					public void run() {
+						typeText.setText("@" + selectedUser + ": ");
+						typeText.requestFocus();
+					}
+				}
+			);
+        }
+	}
+	
+	
+	
+	
+	
+	
+	public static void SUBMIT_ACTION() throws IOException{
+		if(!typeText.getText().equals("")){
+			clientThread.SEND(typeText.getText());
+			typeText.requestFocus();
+			typeText.setText("");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	public static void Initialize(){
+		submit.setEnabled(false);
+		mainWindow.setEnabled(false);
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
 
