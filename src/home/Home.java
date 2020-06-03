ed59ad82131353018c74afd1c8ec814de33fe742
Maximeguenneteau <65347801+Maximeguenneteau@users.noreply.github.com>
2020-06-03 19:02:58 +0200

    update home java

--- src/home/Home.java
+++ src/home/Home.java
@@ -249,5 +249,121 @@ public class Home {
         submit.setEnabled(false);
         HomeWindow.setEnabled(false);
     }
+    
+    public static void BuildLogInWindow(){
 
+    logInWindow.setTitle("Log In");
+
+    ConfigureLogInWindow();
+    LogInWindow_Action();
+    logInWindow.setVisible(true);
+}
+
+
+
+
+
+
+
+public static void ConfigureLogInWindow(){
+    logInWindow.setContentPane(logInWindowGui);
+    logInWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    logInWindow.setMinimumSize(new Dimension(370,150));
+    logInWindow.pack();
+    logInWindow.setLocationRelativeTo(null);
+    try {
+        // 1.6+
+        logInWindow.setLocationByPlatform(true);
+        logInWindow.setMinimumSize(logInWindow.getSize());
+    }
+    catch(Throwable ignoreAndContinue) {
+    }
+
+
+    logInWindowGui.setLayout(new FlowLayout());
+    logInWindowGui.add(logInEnterUsername);
+    logInWindowGui.add(logInUsernameBox);
+    logInWindowGui.add(logInEnterPassword);
+    logInWindowGui.add(logInPasswordBox);
+    logInWindowGui.add(logInEnter);
+}
+
+
+
+
+
+
+
+public static void LogInWindow_Action(){
+    logInEnter.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    LOGIN_ACTION();
+                }
+
+            }
+    );
+
+    logInUsernameBox.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    LOGIN_ACTION();
+                }
+            }
+    );
+}
+
+
+public static void LOGIN_ACTION() {
+    if(!logInUsernameBox.getText().equals("") && !logInPasswordBox.getText().equals("")) {
+        user.setUserName(logInUsernameBox.getText().trim());
+        user.setPassword(logInPasswordBox.getText().trim());
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:h2:" + "./Database/user", "system", "admin");
+            Statement stmt = con.createStatement();
+            String sql = " select * FROM REGISTRATION";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next())
+            {
+                if (user.getUserName().equals(rs.getString(2)) && user.getPassword().equals(rs.getString(3))) {
+                    HomeWindow.setTitle("ChatRoom - "+user.getUserName());
+                    logInWindow.dispose();
+                    //submit.setEnabled(true);
+                    HomeWindow.setEnabled(true);
+                    typeText.requestFocus();
+                    //Connect();
+                    connected = 1;
+                    break;
+                }
+            }
+            if(connected!=1){
+                JOptionPane.showMessageDialog(null, "Please Enter valid login information!");
+            }
+
+            rs.close(); con.close();
+
+
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+        }
+
+
+    }
+    else {
+        JOptionPane.showMessageDialog(null, "Please Enter your complete login information!");
+    }
+}
+
+public static void main(String[] args) throws UnknownHostException, IOException{
+
+    BuildMainWindow();
+    Initialize();
+    BuildLogInWindow();
+
+}
+}
    
