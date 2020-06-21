adb1676f6191917eefb0d29d4704137ff91612cb
BenkemounTom <65448513+BenkemounTom@users.noreply.github.com>
2020-06-21 02:57:03 +0200

    Update registration.java

--- src/registration/registration.java
+++ src/registration/registration.java
@@ -1,46 +1,39 @@
 package registration;
 
 import home.Home;
-import login.Login;
 import user.User;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class registration  {
-    JPanel panel;
-    JLabel user_label, password_label, message;
-    JTextField userName_text;
-    JPasswordField password_text;
-    JButton submit, cancel;
 
     public static User user = new User();
 
-    public static JFrame regWindow = new JFrame();
-    public static JPanel regWindowGui = new JPanel();
-    public static JLabel regEnterUsername = new JLabel("Enter Username");
-    public static JLabel regEnterPassword = new JLabel("Enter Password");
-    public static JTextField regUsernameBox = new JTextField(20);
-    public static JTextField regPasswordBox = new JPasswordField(20);
-    public static JButton register = new JButton("Register");
-
+    private static JFrame regWindow = new JFrame();
+    private static JPanel regWindowGui = new JPanel();
+    private static JLabel regEnterUsername = new JLabel("Enter Username");
+    private static JLabel regEnterPassword = new JLabel("Enter Password");
+    private static JTextField regUsernameBox = new JTextField(20);
+    private static JTextField regPasswordBox = new JPasswordField(20);
+    private static JButton register = new JButton("Sign up");
 
     public static void BuildRegWindow(){
-
-        regWindow.setTitle("Registration");
-
+        regWindow.setTitle("Sign up");
         ConfigureRegWindow();
         RegWindow_Action();
         regWindow.setVisible(true);
     }
 
-    public static void ConfigureRegWindow(){
+    private static void ConfigureRegWindow(){
         regWindow.setContentPane(regWindowGui);
         regWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         regWindow.setMinimumSize(new Dimension(370,150));
@@ -54,7 +47,6 @@ public class registration  {
         catch(Throwable ignoreAndContinue) {
         }
 
-
         regWindowGui.setLayout(new FlowLayout());
         regWindowGui.add(regEnterUsername);
         regWindowGui.add(regUsernameBox);
@@ -63,7 +55,7 @@ public class registration  {
         regWindowGui.add(register);
     }
 
-    public static  void RegWindow_Action(){
+    private static  void RegWindow_Action(){
         register.addActionListener(
                 new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
@@ -74,7 +66,7 @@ public class registration  {
         );
     }
 
-    public static void REGISTER_ACTION() {
+    private static void REGISTER_ACTION() {
         user.setUserName(regUsernameBox.getText());
         user.setPassword(regPasswordBox.getText());
 
@@ -88,11 +80,21 @@ public class registration  {
                 stmt.executeUpdate(sql);
                 JOptionPane.showMessageDialog(null, " Welcome " + user.getUserName());
                 con.close();
-
                 regWindow.dispose();
-                Home.HomeWindow.setTitle("ChatRoom - "+user.getUserName());
 
-                //submit.setEnabled(true);
+                //Create new users' conversation file
+                try {
+
+                    File file = new File(user.getUserName()+".txt");
+                    file.delete();
+                    if(file.createNewFile())System.out.println("Success!");
+                    else System.out.println ("Error, file already exists.");
+                }
+                catch(IOException ioe) {
+                    ioe.printStackTrace();
+                }
+
+                Home.HomeWindow.setTitle(user.getUserName());
                 Home.HomeWindow.setEnabled(true);
                 Home.typeText.requestFocus();
             }
@@ -102,37 +104,6 @@ public class registration  {
             System.err.println(e.getMessage());
         }
     }
-   /* private registration() {
-        // Username Label
-        user_label = new JLabel();
-        user_label.setText("User Name :");
-        userName_text = new JTextField();
-        // Password Label
-        password_label = new JLabel();
-        password_label.setText("Password :");
-        password_text = new JPasswordField();
-        // Submit
-        submit = new JButton("SUBMIT");
-        panel = new JPanel(new GridLayout(3, 1));
-        panel.add(user_label);
-        panel.add(userName_text);
-        panel.add(password_label);
-        panel.add(password_text);
-        message = new JLabel();
-        panel.add(message);
-        panel.add(submit);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Adding the listeners to components..
-        submit.addActionListener(this);
-        add(panel, BorderLayout.CENTER);
-        setTitle("Please Login Here !");
-        setSize(450,350);
-        setVisible(true);
-    }*/
-   // public static void main(String[] args) {
-       // new registration();
-   // }
-
 
     private static boolean alreadyExists(String userName){
         try {
@@ -155,6 +126,4 @@ public class registration  {
         }
         return false;
     }
-
-
 }
